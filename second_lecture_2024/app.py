5c7c39cd8624f4a6b3866b8a1f80f3ad8bf11a67
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2024-09-14 19:50:16 +0300

    add lecture 2 project

---
+++ second_lecture_2024/app.py
@@ -0,0 +1,83 @@
+from decouple import config
+from flask import Flask, request
+from flask_migrate import Migrate
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
+
+app = Flask(__name__)
+
+db_user = config('DB_USER')
+db_password = config('DB_PASSWORD')
+db_host = config('DB_HOST')
+db_port = config('DB_PORT')
+db_name = config('DB_NAME')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+db = SQLAlchemy(app, model_class=Base)
+api = Api(app)
+migrate = Migrate(app, db)
+
+
+class BookModel(db.Model):
+    __tablename__ = 'books'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    author: Mapped[str] = mapped_column(String(50), nullable=False)
+    reader_id: Mapped[int] = mapped_column(ForeignKey("readers.id"), nullable=True)
+
+    reader: Mapped['ReaderModel'] = relationship(back_populates='books')
+
+    def __repr__(self):
+        return f"<{self.pk}> {self.title} from {self.author}"
+
+    def as_dict(self):
+        return {"id": self.id, "title": self.title, "author": self.author}
+
+
+class ReaderModel(db.Model):
+    __tablename__ = 'readers'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+
+    books: Mapped[list['BookModel']] = relationship(back_populates='reader')
+
+
+class BooksResource(Resource):
+    def get(self):
+        # books = BookModel.query.all() # version 2
+        books = db.session.execute(db.select(BookModel)).scalars()  # version 3
+        return [b.as_dict() for b in books]
+
+    def post(self):
+        data = request.get_json()
+        book = BookModel(**data)
+        db.session.add(book)
+        db.session.commit()
+        return book.as_dict(), 201
+
+
+class BookResource(Resource):
+    def get(self):
+        # TODO homework
+        pass
+
+    def put(self):
+        # TODO homework
+        pass
+
+    def delete(self):
+        # TODO homework
+        pass
+
+
+api.add_resource(BooksResource, "/books")
+# TODO Homework - add the Bookresource to respective binding url
